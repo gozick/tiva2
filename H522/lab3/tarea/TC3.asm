f6ceccdd68b5eb1ad08291598f9b2a278846c707
ZurMaD <pablo.diazv@pucp.edu.pe>
2017-09-26 01:19:30 -0500

    Update TC3.asm

--- H522/lab3/tarea/TC3.asm
+++ H522/lab3/tarea/TC3.asm
@@ -19,8 +19,11 @@ AMSEL .equ 0x40025528
 ; Numero de elementos del arreglo
 ELEMENTOS .equ 4
 .text ; El programa debe estar en la sección de código
-arreglo_espacio:	.bss ARREGLOS,40; solo necesitamos 4 variables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+arreglo_espacio:	.bss ARREGLOS,40
+; Asignamos espacio para las variables
 arreglos .word ARREGLOS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	.thumb ; Se programa con instrucciones THUMB y se
 	; usa sintaxis UAL
 	.global main ; Dirección de inicio la etiqueta main
@@ -31,9 +34,9 @@ main:
 	ldr r1, [r2]; leo el dato
 	orr r0, r1, #0x00000020;activo los puertos que deseo
 	str r0, [r2] ; Señal de reloj para GPIOF
-	nop;espero a real activacion
-	nop
-	nop
+	nop;espero
+	nop;espero
+	nop;espero
 	ldr r0, KEYLOCK
 	str r0, [r3, #LOCK] ; se escribe la llave para desbloquear PD7
 	ldr r0, [r3, #CR]
@@ -47,125 +50,118 @@ main:
 	mov r0, #0x0000001F;activamos los puertos 1,2 y 3
 	str r0, [r3, #DEN];guardamos los datos
 	ldr r0,[r3,#DIR]
-
-
-	ldr r0,[r3,#AFSEL]
+	ldr r0,[r3,#AFSEL];Activamos el AFSEL
 	and r0,r0,#0xE0
 	str r0,[r3,#AFSEL]
-
 	ldr r0, [r3, #PUR]
-	orr r0, r0, #0x00000011 ; activa resistencias de pull-up
+	orr r0, r0, #0x011 ; activa resistencias de pull-up
 	str r0, [r3, #PUR]
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 guardar_arreglos:
-	ldr r7,arreglos
-	mov r6,#1
+	ldr r7,arreglos ;GUARDAMOS LOS VALORES DEL ARREGLO
+	mov r6,#1			;#1
 	str r6,[r7]
-	mov r6,#3
+	mov r6,#3			;#3
 	str r6,[r7,#0x1]
-	mov r6,#5
+	mov r6,#5			;#5
 	str r6,[r7,#0x2]
-	mov r6,#7
+	mov r6,#7			;#7
 	str r6,[r7,#0x3]
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 sw2_presionado:
 	ldr r1,[r3,#DATA]
-	and r1, #0x01;PF4
-	cbz r1, sw2_soltado; si no es cero enviar a sw1 presionado
+	and r1, #0x01		;ESTADO SW2
+	cbz r1, sw2_soltado ;MIENTRAS NO SE PRESIONE SW2
 	b sw2_presionado;bucle
 sw2_soltado:
 	ldr r1,[r3,#DATA]
 	and r1,r1, #0x01;PF4
-	cbnz r1, prender_led_azul; si no es cero enviar a sw1 presionado
+	cbnz r1, prender_led_azul;MIENTRAS NO SE SUELTE SW2
 	b sw2_soltado;bucle
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 prender_led_azul:
-	; Conmutar LED Azul (PF2)
-	ldr r1, [r3, #DATA] ; r1 <-- [dirección base + offset(DATA)]
-	mov r1,#0x4;
+	ldr r1, [r3, #DATA]
+	mov r1,#0x4			;PRENDEMOS LED AZUL
 	str r1, [r3, #DATA]
 	mov r1,#100
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 retardo_led_1:
-	mov r2,#10000;10000 por cada 1 de r1
+	mov r2,#10000			;GENERAMOS UN RETARDO
 retardo_led_2:
 	sub r2,r2,#0x1
-	;cbz r2,retardo_led_2
 	cmp r2,#0x0
 	bne retardo_led_2
 	sub r1,r1,#1
 	cmp r1,#0
 	bne retardo_led_1
-	;cbz r1,sw1_presionado
-	ldr r7,arreglos; llamamos la palabra arreglo
-	mov r0,#4
+	ldr r7,arreglos; LEEMOS EL PUNTERO DEL ARREGLO
+	mov r0,#4		; DECIMOS EL NUMERO DE ELEMENTOS DEL ARREGLO
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 sw1_presionado:
 	ldr r4,[r3,#DATA]
-	and r4, #0x10;PF4
-	cbz r4, sw1_soltado; si no es cero enviar a sw1 presionado
+	and r4, #0x10		;VEMOS EL ESTADO DEL SW1
+	cbz r4, sw1_soltado ;SI NO ES PRESIONADO
 	b sw1_presionado;bucle
 sw1_soltado:
 
 	ldr r4,[r3,#DATA]
 	and r4, #0x10;PF4
-	cbnz r4, j_arreglo; si no es cero enviar a sw1 presionado
-	b sw1_soltado;bucle
+	cbnz r4, j_arreglo  ;SI NO ES SOLTADO
+	b sw1_soltado
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 j_arreglo:
-	ldr r5,[r7]
+	ldr r5,[r7] 		;LEE EL ESTADO DEL ARREGLO
 j_pregunta:
-	and r5,#0XFF
-	cmp r5,#0
-	bne prender_led_rojo
-	beq siguiente_i
+	and r5,#0XFF		;SOLO TOMAMOS 1 BYTE
+	cmp r5,#0				;R5 SIRVE PARA LOS PULSOS
+	bne prender_led_rojo	;SI TODAVIA HAY PULSOS POR HACER
+	beq siguiente_i			;SIGUIENTE NUMERO EN EL ARREGLO
 	nop
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-prender_led_rojo:
-	ldr r4,[r3,#DATA]
-	mov r4,#0x02
-	str r4,[r3,#DATA]
+prender_led_rojo:			;PRENDEMOS
+	ldr r4,[r3,#DATA]		;LED ROJO Y APAGAMOS
+	mov r4,#0x02			;LOS OTROS
+	str r4,[r3,#DATA]		;LEDS
 	mov r1,#100
-retardo_led_11:
-	mov r2,#10000;10000 por cada 1 de r1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+retardo_led_11:				;GENERAMOS
+	mov r2,#10000			;RETARDOS
 retardo_led_22:
 	sub r2,r2,#0x1
-	;cbz r2,retardo_led_2
 	cmp r2,#0x0
 	bne retardo_led_22
 	sub r1,r1,#1
 	cmp r1,#0
 	bne retardo_led_11
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 apagar_led_rojo:
-	mov r4, #0x0
-	str r4,[r3,#DATA]
-	;cbz r1,sw1_presionado
+	mov r4, #0x0			;APAGAMOS
+	str r4,[r3,#DATA]		;TODOS LOS LEDS
 	mov r1,#100
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 retardo_led_111:
-	mov r2,#10000;10000 por cada 1 de r1
-retardo_led_222:
+	mov r2,#10000			;RETARDO
+retardo_led_222:			;ANTES DE APAGAR
 	sub r2,r2,#0x1
-	;cbz r2,retardo_led_2
 	cmp r2,#0x0
 	bne retardo_led_222
 	sub r1,r1,#1
 	cmp r1,#0
 	bne retardo_led_111
-	subs r5,#1
-	b j_pregunta
+	subs r5,#1			;RESTAMOS R5 Y VEMOS SI
+	b j_pregunta		;YA SE TERMINO LOS PULSOS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-siguiente_i:
-	add r7,#1
-	subs r0,#1
-	cmp r0,#0
-	beq reiniciar_i
+siguiente_i:			;AUMENTAMOS EL VALOR
+	add r7,#1			;DEL PUNTERO DEL ARREGLO
+	subs r0,#1			;RESTAMOS LA CANTIDAD DE
+	cmp r0,#0			;DATOS QUE HAY EN EL
+	beq reiniciar_i		;ARREGLO
 	bne sw1_presionado
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 reiniciar_i:
-	mov r0, #4
-	ldr r7,arreglos
-	b sw1_presionado
+	mov r0, #4			;REINICIAMOS
+	ldr r7,arreglos		;EL NUMERO DE DATOS DEL ARREGLO
+	b sw1_presionado	;VOLVEMOS A VER ESTADO SW1
 ; Registros base
 GPIO_PORTE_BASE: .word 0x40024000
 GPIO_PORTF_BASE: .word 0x40025000
