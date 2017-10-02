787449fa8e9e180829ac4fc1bfa1ccb9156c73cd
ZurMaD <pablo.diazv@pucp.edu.pe>
2017-10-01 20:44:30 -0500

    Update BH1750.c

--- robot_mapeo_luz/I2C_LUZ/BH1750.c
+++ robot_mapeo_luz/I2C_LUZ/BH1750.c
@@ -21,7 +21,7 @@
 38400	26	2.666
 57600	17	23.1111
 115200	8	43.555
-*/
+ */
 void ConfiguraUART_PC(void){
 	SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0;	// Habilitamos reloj para el UART0
 	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA;	// Habilitamos reloj para GPIOA
@@ -72,8 +72,9 @@ void I2C1_BH1750(void){
 /********************* FIN PRENDER I2C1    ************************************/
 
 /********************    TRANSMISION POR I2C  ************************************/
-uint8_t leer_I2C_BH1750(uint8_t direccion_esclavo, uint8_t registro){
-	uint8_t dato,estado;
+uint16_t leer_I2C_BH1750(uint8_t direccion_esclavo, uint8_t registro){
+	uint8_t dato1,dato2,estado;
+	uint16_t medidaluz;
 	//I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x7;							// Condicion de inicio
 	do{
 
@@ -86,20 +87,21 @@ uint8_t leer_I2C_BH1750(uint8_t direccion_esclavo, uint8_t registro){
 		do{
 			estado= I2C1_MCS_R;
 		}while(estado & 0x01 != 0); 								//Se espera que el bit busy se ponga en 0
-	}while( (I2C1_MCS_R & 0x02) != 0);								// si hay error vuelve a realizar 
-																	//todos desde el inicio, sino sigue
+	}while( (I2C1_MCS_R & 0x02) != 0);								// si hay error vuelve a realizar
+	//todos desde el inicio, sino sigue
 	do{
 		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) + (direccion_esclavo<<1) + 1; // se escribe la direccion del esclavo y se pone como lectura
-		I2C1_MCS_R= (I2C1_MCS_R & ~0x1F)| 0x7;
+		I2C1_MCS_R= (I2C1_MCS_R & ~0x1F)| 0x7;						//Condicion de parada
 		do{
 			estado= I2C1_MCS_R;
 		}while(estado & 0x01 != 0); 					// se espera que el bit busy se ponga en 0
 	}while( (I2C1_MCS_R & 0x02) != 0);					// si hay error vuelve a realizar todos desde el inicio, sino sigue
-	dato = I2C1_MDR_R & 0xFF; 							// se lee la data del registro escogido
+	dato1 = I2C1_MDR_R & 0xFF; 							// se lee la data del registro escogido
 	//ACK
-	
+	dato2 = I2C1_MDR_R & 0xFF;
 	//I2C1_MCS_R= (I2C1_MCS_R & ~0x1F)| 0x4;			// condicion de parada;
-	return dato;
+	medidaluz=(dato1<<8)+dato2;
+	return medidaluz;
 }// fin leer_I2C
 
 void escribir_I2C_BH1750 (uint8_t direccion_esclavo , uint8_t registro , uint8_t valor){
@@ -125,191 +127,60 @@ void escribir_I2C_BH1750 (uint8_t direccion_esclavo , uint8_t registro , uint8_t
 /******************** FIN TRANSMISION POR I2C  ************************************/
 
 /******************** CONVERSION DE DATOS ************************************/
-//unsigned char Datos[7]; // 3 digitos para enviar
-//void NumToChar(long n){
-//
-//	Datos[0] = n/1000000 + 0x30; // Centenas
-//	n = n%1000000; // n ahora esta entre 0 y 999999
-//	Datos[1] = n/100000 + 0x30; // Centenas
-//	n = n%100000; // n ahora esta entre 0 y 99999
-//	Datos[2] = n/10000 + 0x30; // Centenas
-//	n = n%10000; // n ahora esta entre 0 y 9999
-//	Datos[3] = n/1000 + 0x30; // Centenas
-//	n = n%1000; // n ahora esta entre 0 y 999
-//	Datos[4] = n/100 + 0x30; // Centenas
-//	n = n%100; // n ahora esta entre 0 y 99
-//	Datos[5] = n/10 + 0x30; // Decenas
-//	n = n%10; // n ahora esta entre 0 y 9
-//	Datos[6] = n + 0x30; // Unidades
-//	return Datos;
-//}
+unsigned char Datos[5]; // 5 digitos para expresar la medida de luz
+void NumerotoString(uint16_t n){
+
+	//	Datos[0] = n/1000000 + 0x30; // Centenas
+	//	n = n%1000000; // n ahora esta entre 0 y 999999
+	//	Datos[1] = n/100000 + 0x30; // Centenas
+	//	n = n%100000; // n ahora esta entre 0 y 99999
+	Datos[0] = n/10000 + 0x30; 					// Decenas de miles
+	n = n%10000; 								// n ahora esta entre 0 y 9999
+	Datos[1] = n/1000 + 0x30; 					// Miles
+	n = n%1000;									// n ahora esta entre 0 y 999
+	Datos[2] = n/100 + 0x30; 					// Centenas
+	n = n%100;									// n ahora esta entre 0 y 99
+	Datos[3] = n/10 + 0x30; 					// Decenas
+	n = n%10; 									// n ahora esta entre 0 y 9
+	Datos[4] = n + 0x30;						// Unidades
+	return Datos;
+}
 /********************FIN CONVERSION DE DATOS **********************************/
-//_________________________________MAIN__________________________________//
+/********************          MAIN          **********************************/
 void main (void){
 	ConfiguraUART_PC(); 			// COMUNICACION UART CON PC
 	I2C1_BH1750(); 					//CONFIGURA EL I2C PA6YPA7
-	uint8_t dato;
+	uint16_t luz;
+	int i,contador=0;							//Se usara para retardo
 	uint8_t direccion_esclavo=0b0100011;				//7 digitos
 	uint8_t registro=0b00010001;						//8 digitos
-	dato=leer_I2C_BH1750(direccion_esclavo, registro);	//Escribimos el esclavo
-														//escribimos el registro
-														//para leer data
+	uint8_t power_on=0b00000001;						//prender el sensor
 
-//	I2C1(); // PRENDER I2C1
-//	//_____________________________1.CALIBRACION_________________________________//
-//	//Declaracion de variables
-//	uint32_t i,seg;
-//	short dato1,dato2,datoreal;
-//	short AC1,AC2,AC3;
-//	short B1,B2,MB,MC,MD;
-//	short oss; // oss =0,1,2,3
-//	unsigned short AC4,AC5,AC6;
-//	long UT,UP;
-//	long altitud, presion;
-//	long x1,x2,x3,B5,T;
-//	long B6,B3,p;
-//	unsigned long B4,B7;
-//	uint8_t mensaje2[]= "Presion(Pa): ";
-//	uint8_t mensaje3[]= "\n\r ";
-//	uint8_t espacio[]= " ";
-//	uint8_t mensaje4[]= "Altitud(msnm): ";
-//	dato1=0;
-//	dato2=0;
-//	datoreal=0;
-//	AC1=0;
-//	AC2=0;
-//	AC3=0;
-//	AC4=0;
-//	AC5=0;
-//	AC6=0;
-//	B1=0;
-//	B2=0;
-//	MB=0;
-//	MC=0;
-//	MD=0;
-//	UT=0;
-//	UP=0;
-//	altitud=0;
-//	presion=0;
-//	x1=0;
-//	x2=0;
-//	x3=0;
-//	B5=0;
-//	B3=0;
-//	T=0;
-//	B6=0;
-//	p=0;
-//	B4=0;
-//	B7=0;
-//	seg=1;
-//	oss=0;
-//	//Leer datos de calibracion AC1
-//	dato1=leer_I2C(0x77,0xAA);
-//	dato2=leer_I2C(0x77,0xAB);
-//	AC1=(dato1<<8)+dato2;
-//	//Leer datos de calibracion AC2
-//	dato1=leer_I2C(0x77,0xAC);
-//	dato2=leer_I2C(0x77,0xAD);
-//	AC2=(dato1<<8)+dato2;
-//	//Leer datos de calibracion AC3
-//	dato1=leer_I2C(0x77,0xAE);
-//	dato2=leer_I2C(0x77,0xAF);
-//	AC3=(dato1<<8)+dato2;
-//	//Leer datos de calibracion AC4
-//	dato1=leer_I2C(0x77,0xB0);
-//	dato2=leer_I2C(0x77,0xB1);
-//	AC4=(dato1<<8)+dato2;
-//	//Leer datos de calibracion AC5
-//	dato1=leer_I2C(0x77,0xB2);
-//	dato2=leer_I2C(0x77,0xB3);
-//	AC5=(dato1<<8)+dato2;
-//	//Leer datos de calibracion AC6
-//	dato1=leer_I2C(0x77,0xB4);
-//	dato2=leer_I2C(0x77,0xB5);
-//	AC6=(dato1<<8)+dato2;
-//	//Leer datos de calibracion B1
-//	dato1=leer_I2C(0x77,0xB6);
-//	dato2=leer_I2C(0x77,0xB7);
-//	B1=(dato1<<8)+dato2;
-//	//Leer datos de calibracion B2
-//	dato1=leer_I2C(0x77,0xB8);
-//	dato2=leer_I2C(0x77,0xB9);
-//	B2=(dato1<<8)+dato2;
-//	//Leer datos de calibracion MB
-//	dato1=leer_I2C(0x77,0xBA);
-//	dato2=leer_I2C(0x77,0xBB);
-//	MB=(dato1<<8)+dato2;
-//	//Leer datos de calibracion MC
-//	dato1=leer_I2C(0x77,0xBC);
-//	dato2=leer_I2C(0x77,0xBD);
-//	MC=(dato1<<8)+dato2;
-//	//Leer datos de calibracion MD
-//	dato1=leer_I2C(0x77,0xBE);
-//	dato2=leer_I2C(0x77,0xBF);
-//	MD=(dato1<<8)+dato2;
-//
-//	while(1){
-//		//___________LEER TEMPERATURA UT_____________//
-//
-//		escribir_I2C(0x77,0xF4,0x2E);
-//		for (i=0;i<100000;i++); //wait 4.5ms
-//		dato1=leer_I2C(0x77,0xF6);
-//		dato2=leer_I2C(0x77,0xF7);
-//		UT=((dato1<<8)+dato2);
-//		//______________LEER PRESION UP______________//
-//		escribir_I2C(0x77,0xF4,(0x34+(oss<<6)));
-//		for (i=0;i<450000;i++); //wait 4.5ms
-//		dato1=leer_I2C(0x77,0xF6);
-//		dato2=leer_I2C(0x77,0xF7);
-//		UP=(((dato1<<16)+(dato2<<8))>>(8-oss));
-//		//_______CALCULAR LA REAL TEMPERATURA________//
-//		x1=(UT-AC6)*AC5/pow(2,15);
-//		x2=MC*pow(2,11)/(x1+MD);
-//		B5=x1+x2;
-//		T=(B5+8)/pow(2,4);
-//
-//		//__________TRANSFORMAR PRESION_____________//
-//		B6=B5-4000;
-//		x1=(B2*(B6*B6/pow(2,12)))/pow(2,11);
-//		x2=AC2*B6/pow(2,11);
-//		x3=x1+x2;
-//		B3=(((AC1*4+x3)<<oss)+2)/4;
-//		x1=AC3*B6/pow(2,13);
-//		x2=(B1*(B6*B6/pow(2,12)))/pow(2,16);
-//		x3=((x1+x2)+2)/pow(2,2);
-//		B4=AC4*(unsigned long)(x3+32768)/pow(2,15);
-//		B7=((unsigned long)UP-B3)*(50000>>oss);
-//		if (B7<0x80000000){
-//			p=(B7*2)/B4;
-//		}
-//		else {
-//			p=(B7/B4)*2;
-//		}
-//		x1=(p/pow(2,8))*(p/pow(2,8));
-//		x1=(x1*3038)/pow(2,16);
-//		x2=(-7357*p)/pow(2,16);
-//		p=p+(x1+x2+3791)/pow(2,4);
-//		//__________TRANSFORMAR MSNM_____________//
-//		double altitud;
-//		double v1,v2;
-//		v1=(p*10000)/101325;
-//		v1=v1/10000;
-//		v2=1/5.225;
-//		altitud=44330*(1-pow(v1,v2));
-//		//____TRANSFORMAR PARA ENVIAR POR UART______//
-//		//______________LIMPIAR REGISTRO____________//
-//		for (i=0;i<7;i++){
-//			Datos[i]=0;
-//		}
-//		NumToChar(p);
-//		txmens_uart0(mensaje2);//PRESION
-//		txmens_uart0(Datos);//VALOR DE PRESION
-//		txmens_uart0(espacio);//espacio
-//		txmens_uart0(mensaje4);//ALTITUD
-//		NumToChar(altitud);
-//		txmens_uart0(Datos);//VALOR DE ALTITUD
-//		txmens_uart0(mensaje3);//SIGUIENTE LINEA
-////
-////		for (i=0;i<100000*seg;i++); //wait 4.5ms
-//	}//FIN WHILE 1
-}//______________FIN__MAIN___________________//
+	uint8_t lectura2[]="\n\r";								//Enter
+	uint8_t unidad_luz[]=" [ lx ]\n\r"	;				//Unidades del sensor de luz
+	uint8_t lectura[]="Lectura [";
+	uint8_t lectura3[]="] :";
+
+	while(1){											//Siempre ejecutandose
+		luz=leer_I2C_BH1750(direccion_esclavo, power_on);	//Escribimos el esclavo
+		luz=leer_I2C_BH1750(direccion_esclavo, registro);	//Escribimos el esclavo
+		for (i=0;i<400000;i++);
+		luz=luz/1.2;
+		/////////////////////////////////////////////////////////////////////////
+		NumerotoString(luz);							//la variable datos es global
+		txmens_uart_PC(lectura);
+		txcar_uart_PC(contador+48);						//offset por ser ascii
+		txmens_uart_PC(lectura3);						//Lectura numero 3
+		txmens_uart_PC(Datos);
+		txmens_uart_PC(unidad_luz);						//Fin de todo el mensaje
+		Datos[0]=0;
+		Datos[1]=0;
+		Datos[2]=0;
+		Datos[3]=0;
+		Datos[4]=0;
+		/////////////////////////////////////////////////////////////////////////
+		contador++;
+		if (contador==10)contador=0;					//Solo secuendia de 0 a 9
+	}
+}
+/********************      FIN  MAIN          **********************************/
