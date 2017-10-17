4ebec2dba109eccbea4eedcfa9ca082a6ea43014
ZurMaD <pablo.diazv@pucp.edu.pe>
2017-10-16 20:21:42 -0500

    Update BH1750.c

--- robot_mapeo_luz/I2C_LUZ/BH1750.c
+++ robot_mapeo_luz/I2C_LUZ/BH1750.c
@@ -14,6 +14,17 @@
 #include "tm4c123gh6pm.h"
 #include <math.h>
 
+#define NumeroIntentosMax 5				// Numero maximo de intentos al enviar
+//#define I2C_MCS_ACK		0x8				// Acknowledge prendido
+//#define I2C_MCS_DATACK	0x8				// Acknowledge data
+//#define I2C_MCS_ADRACK	0x4				// Acknowledge Address
+//#define I2C_MCS_STOP	0x4				// Generar Stop
+//#define I2C_MCS_START	0x2				// Generate Start
+//#define I2C_MCS_ERROR	0x2				// Error
+//#define I2C_MCS_RUN		0x1				// I2C prendido
+//#define I2C_MCS_BUSY	0x1				// I2C ocupado
+//#define I2C_MCR_MFE		0x10			// I2C Master funcion prendido
+
 /*********************COMUNICACION CON PC************************************/
 /*
 9600	104	10
@@ -53,90 +64,68 @@ void txmens_uart_PC(uint8_t mens[]){
 
 /*********************PRENDER I2C1 PARA BH1750*********************************/
 void I2C1_BH1750(void){
-	SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R1;			// habilita el reloj del I2C1
+	SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R1;					// habilita el reloj del I2C1
 	while((SYSCTL_PRI2C_R & SYSCTL_PRI2C_R1)==0);
-	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0 ;// habiilita el reloj del puerto A
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0 ;				// habiilita el reloj del puerto A
 	while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R0)==0);
-	GPIO_PORTA_AFSEL_R |= 0xC0;				// se activan funciones alternativas
+	GPIO_PORTA_AFSEL_R |= 0xC0;								// se activan funciones alternativas
 	GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & ~0xFF000000) + 0x33000000;
-	GPIO_PORTA_ODR_R |= 0x80; 				// se configura el sda como open-drain
-	GPIO_PORTA_ODR_R &= ~0x40;				// se desactiva el scl
-	GPIO_PORTA_AMSEL_R |= 0xC0;				// desactiva funcion analogica
-	GPIO_PORTA_DEN_R |= 0xC0;				// Función digital activada
-	I2C1_MCR_R |= 0x10;						// se incializa el maestro
-	I2C1_MTPR_R = (7<<0); // configure for 100 kbps clock;
+	GPIO_PORTA_ODR_R |= 0x80; 								// se configura el sda como open-drain
+	GPIO_PORTA_ODR_R &= ~0x40;								// se desactiva el scl
+	GPIO_PORTA_AMSEL_R |= 0xC0;								// desactiva funcion analogica
+	GPIO_PORTA_DEN_R |= 0xC0;								// Función digital activada
+	I2C1_MCR_R |= 0x10;										// se incializa el maestro
+	I2C1_MTPR_R = (24);									// configure for 100 kbps clock;
 	// se configura la velocidad MTPR = (System Clock/(2*(SCL_LP + SCL_HP)*SCL_CLK))-1;
-	I2C1_MCS_R &= ~0x10;					// Se pone el bit HS en 0, se descativa high speedmode
+	I2C1_MCS_R &= ~0x10;									// Se pone el bit HS en 0, se descativa high speedmode
 }//fin I2C1
 /********************* FIN PRENDER I2C1    ************************************/
 
 /********************    TRANSMISION POR I2C  ************************************/
-//uint8_t leer_I2C_BH1750(uint8_t direccion_esclavo, uint8_t registro){
-//	uint8_t dato,estado;
-//	I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x7;							// Condicion de inicio
-//	do{
-//		while(I2C1_MCS_R&0x00000001){}; // esperar I2C ready
-//		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) +						// se escribe la direccion
-//					(direccion_esclavo<<1);							// del esclavo y se pone como lectura
-//		//ACK
-//		I2C1_MDR_R = (I2C1_MDR_R & ~0xFF) + registro;				//se escribe la direccion del registro que se va a leer
-//		I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x3;						// condicion de inicio
-//		do{
-//			estado= I2C1_MCS_R;
-//		}while(estado & 0x01 != 0); 								//Se espera que el bit busy se ponga en 0
-//	}while( (I2C1_MCS_R & 0x02) != 0);								// si hay error vuelve a realizar
-//	//todos desde el inicio, sino sigueasfasfasfasf
-//	do{
-//		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) + (direccion_esclavo<<1) + 1; // se escribe la direccion del esclavo y se pone como lectura
-//		I2C1_MCS_R= (I2C1_MCS_R & ~0x1F)| 0x7;						//Condicion de parada
-//		do{
-//			estado= I2C1_MCS_R;
-//		}while(estado & 0x01 != 0); 					// se espera que el bit busy se ponga en 0
-//	}while( (I2C1_MCS_R & 0x02) != 0);					// si hay error vuelve a realizar todos desde el inicio, sino sigue
-//	dato = I2C1_MDR_R & 0xFF; 							// se lee la data del registro escogido
-//	//ACK
-//	//dato2 = I2C1_MDR_R & 0xFF;
-//	I2C1_MCS_R= (I2C1_MCS_R & ~0x1F)| 0x4;			// condicion de parada;
-//	//medidaluz=(dato1<<8)+dato2;
-//	return dato;
-//}// fin leer_I2C
 uint16_t leer_I2C_BH1750(uint8_t direccion_esclavo){
-	uint8_t datoH=1,datoL=0,estado=0;
-	uint8_t AckEsclavo=0;
-	I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x1;							// Bit RUN 1
-	do{																// MCS modo lectura
-		while(I2C1_MCS_R&0x00000001){}; 							// Esperar I2C
-		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) +						// Limpiamos registro
-					(direccion_esclavo<<1)+1;						// Direccion de esclavo +W/R=1
-		AckEsclavo=I2C1_MCS_R;										// Ack Esclavo Modo lectura
-		I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x7;						// Estado ERROR & BUSY
-		do{
-			estado= I2C1_MCS_R;
-		}while(estado & 0x01 != 0); 								// Hasta que no este BUSY
-	}while( (I2C1_MCS_R & 0x02) != 0);								// Si existe error reenvia denuevo
-	datoH = I2C1_MDR_R & 0xFF; 										// Leemos Hight Byte
-	I2C1_MCS_R|=(I2C1_MCS_R & ~0x1F)| 0x8;							// Generamos ACK del maestro
-	datoL= I2C1_MDR_R & 0xFF; 										// Leemos Low Byte
-	I2C1_MCS_R&=~0x8;												// Generamos ACK negativo
-	I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x4;							// BIT DE PARADA
+	/*
+	 * Esta función recibe dos bytes y retorna un numero
+	 * sin signo de 16 bits que representa la medicion del sensor
+	 * pero aun falta modificar este dato para que sea real
+	 */
+	uint8_t datoH=0,datoL=0;
+	uint8_t intento=1;
+	//I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x1;					// Bit RUN 1
+	do{															// MCS modo lectura
+		while(I2C1_MCS_R&I2C_MCS_BUSY); 						// Esperar I2C
+		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) +					// Limpiamos registro
+				(direccion_esclavo<<1);							// Direccion de esclavo +W/R=1
+		I2C1_MSA_R|=0x1;										// Como lectura
+		I2C1_MCS_R=(0
+				|I2C_MCS_ACK									// ACK positivo activado
+				|I2C_MCS_START									// Generar start/restart
+				|I2C_MCS_RUN);									// Modo master prendido
+		while(I2C1_MCS_R&I2C_MCS_BUSY); 						// Esperar I2C
+		datoH=(I2C1_MDR_R&0xFF);								// Recibo Hight Byte del sensor
+		I2C1_MCS_R=(0
+				|I2C_MCS_STOP									// ACK positivo activado
+				//|I2C_MCS_START								// Generar start/restart
+				|I2C_MCS_RUN);									// Modo master prendido
+		while(I2C1_MCS_R&I2C_MCS_BUSY); 						// Esperar I2C
+		datoL=(I2C1_MDR_R&0xFF);								// Recibo Hight Byte del sensor
+		intento++;												// Aumentar el contador
+	}while(((I2C1_MCS_R&(I2C_MCS_ADRACK|I2C_MCS_ERROR))!=0)&&(intento<=NumeroIntentosMax));//Mientras sea menor el intento
 	return (datoH<<8)+datoL;										// Devolvemos valor medido
 }// fin leer_I2C
-void escribir_I2C_BH1750 (uint8_t direccion_esclavo ,uint8_t opecode){
-	char estado=0;													// Estado nos indica los bits
-																	// ERROR y BUSY del Master al enviar
-	uint8_t AckEsclavo=0;
-	do{																// MCS EN MODO WRITE
-		I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x1;						// BIT DE RUN
-		I2C1_MCS_R&=~0x1;											// En modo Write Borramos bit 0
-		I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) + (direccion_esclavo<<1); // Direccion de esclavo con W/R=0
-		AckEsclavo=I2C1_MCS_R;										// Ack Esclavo Modo lectura
-		I2C1_MDR_R = (I2C1_MDR_R & ~0xFF) + opecode;				 // Enviamos el opcode
-		AckEsclavo=I2C1_MCS_R;										// Ack Esclavo Modo lectura
-		I2C1_MCS_R= (I2C1_MCS_R & ~0x17)| 0x4;						// BIT DE PARADA
-		do{
-			estado= I2C1_MCS_R;
-		}while((estado & 0x01) != 0); 								// Se queda aqui hasta que no este busy
-	}while( (I2C1_MCS_R & 0x02) != 0);								// Si existe error repetir el envio
+uint32_t escribir_I2C_BH1750 (uint8_t direccion_esclavo ,uint8_t opecode){
+	/* Esta función los Measurement code = opecode
+	 * según datasheet de BH1750
+	 */
+	while(I2C1_MCS_R&I2C_MCS_BUSY); 								// Esperar a I2C libre
+	I2C1_MSA_R =  (I2C1_MSA_R & ~0xFF) + (direccion_esclavo<<1);	// Direccion de esclavo con W/R=0
+	I2C1_MSA_R&=~0x1;												// Modo escritura
+	I2C1_MDR_R=opecode&0xFF;										// Enviamos opecode
+	I2C1_MCS_R =(0
+			|I2C_MCS_STOP											// Generar un stop
+			|I2C_MCS_START											// Generar Start/ Restart
+			|I2C_MCS_RUN);											// Prender modo Master
+	while(I2C1_MCS_R&I2C_MCS_BUSY); 								// Esperar a I2C libre
+	return (I2C1_MCS_R&(I2C_MCS_DATACK|I2C_MCS_ADRACK|I2C_MCS_ERROR));	// Retornar algun error
 }//fin escribir_I2C_BH1750
 /******************** FIN TRANSMISION POR I2C  ************************************/
 
@@ -160,7 +149,16 @@ void NumerotoString(uint16_t n){
 	return Datos;
 }
 /********************FIN CONVERSION DE DATOS **********************************/
-
+void retardo_ms (uint32_t milisegundos) {
+	/* Esta función genera un retardo exacto contado en milisegundos
+	 * solo basta ingresar y generará un retardo con error de +/- 1/16millones
+	 */
+	NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE; 										//Deshabilitamos Systick
+	NVIC_ST_RELOAD_R = (NVIC_ST_RELOAD_R & ~0xFFFFFF) + (16000*milisegundos - 1); 	//Programamos valor de recarga
+	NVIC_ST_CURRENT_R = (NVIC_ST_CURRENT_R & ~0xFFFFFF) + 1; 						//Borramos la cuenta
+	NVIC_ST_CTRL_R |= (NVIC_ST_CTRL_ENABLE + NVIC_ST_CTRL_CLK_SRC); 				//Habilitamos Systick
+	while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT)==0);
+}
 /********************          MAIN          **********************************/
 void main (void){
 	ConfiguraUART_PC(); 								// COMUNICACION UART CON PC
@@ -169,15 +167,18 @@ void main (void){
 	int i,contador=0;									// Se usara para retardo
 	uint8_t direccion_esclavo=0b0100011;				// 7 digitos
 	uint8_t power_on=0b00000001;						// Prender el sensor
-	uint8_t MeasurementCode=0b00010011;					// Mesaurement Command
+	uint8_t MeasurementCode=0b00010000;					// Mesaurement Command
 	uint8_t unidad_luz[]=" [ lx ]\n\r"	;				// Unidades del sensor de luz
 	uint8_t lectura[]="Lectura [";						// TEXTO
 	uint8_t lectura3[]="] :";							// TEXTO
+	uint32_t error=0;
+	error=escribir_I2C_BH1750(direccion_esclavo, power_on);	// Prendemos el sistema
+	error=escribir_I2C_BH1750(direccion_esclavo, MeasurementCode);	// Prendemos el sistema
 	while(1){												// Siempre ejecutandose
-		escribir_I2C_BH1750(direccion_esclavo, power_on);	// Prendemos el sistema
-		for (i=0;i<1000;i++);								// Retardo para siguiente lectura
-		escribir_I2C_BH1750(direccion_esclavo, MeasurementCode);	// Comando Measurement Command
-		for (i=0;i<100000;i++);								// Retardo entre escritura y lectura
+
+		//retardo_ms(120);									// Retardo para siguiente lectura
+		//escribir_I2C_BH1750(direccion_esclavo, MeasurementCode);	// Comando Measurement Command
+		//retardo_ms(10);									// Retardo para siguiente lectura
 		luz=leer_I2C_BH1750(direccion_esclavo);				// Leemos Hight y Low Byte
 		luz=luz/1.2;										// Calculamos la real medicion
 		/////////////////////////////////////////////////////////////////////////
