42716140cf22df9a52566dc2e61b2e7f4b36fac8
ZurMaD <pablo.diazv@pucp.edu.pe>
2017-10-31 23:46:12 -0500

    Update and rename H522/lab4/tarea/tc4.c to  H522/ lab4/ tarea/tc4.c

--- H522/ lab4/ tarea/tc4.c
+++ H522/ lab4/ tarea/tc4.c
@@ -12,9 +12,39 @@
 #include "tm4c123gh6pm.h"
 #include <math.h>
 
-unsigned char Datos[5];
+uint8_t msg1[]="!BIENVENIDO! MAQUINA XY DE CORTE EN ESPERA DE COMANDO \n\r";
+uint8_t msg2_1[]="INTRODUZCA LA POSICION ";
+uint8_t msg2_2[]="(X, Y) DEL POLIGONO DE CORTE EN CM: \n\r";
+uint8_t msg3_1[]="SE INTRODUJO LA COORDENADA X = ";
+uint8_t msg3_2[]="CM, FALTA LA COORDENADA Y \n\r";
+uint8_t msg4_1[]="COORDENADA X INCORRECTA, VUELVA A INTRODUCIRLA \n\r";
+uint8_t msg4_2[]="COORDENADA Y INCORRECTA, VUELVA A INTRODUCIRLA \n\r";
+uint8_t msg5_1[]="POSICION ";
+uint8_t msg5_2[]=" (";
+uint8_t msg5_3[]=",";
+uint8_t msg5_4[]=") CORRECTAMENTE INGRESADA \r\n";
+uint8_t msg6[]=" NUMERO DE POSICIONES INSUFICIENTES PARA EJECUTAR EL CORTE\n\r";
 
-void ConfiguraUART_PC (void){//***************CONFIGURAR UART0********************//
+uint8_t msg7[]="PROCESO DE CORTE INICIADO, CODIGO G: \n\r";
+uint8_t msg8[]=" M03 \n\r";
+uint8_t msg9[]=" M00 \n\r";
+uint8_t msg11_1[]=" G00 X";
+uint8_t msg11_2[]=" Y";
+uint8_t msg11_3[]="\n\r";
+uint8_t msg11_4[]=" G01 X";
+uint8_t msg12_1[]=" G01 Z-1\n\r";
+uint8_t msg12_2[]=" G01 Z1\n\r";
+uint8_t msg13[]="N";
+uint8_t fin[]="PROCESO DE CORTE COMPLETADO CON EXITO\n\r";
+
+uint32_t contadorCorte=1;
+uint32_t banderaX=0,banderaY=0;
+uint32_t cantMaxPosic=20;
+int numeroX=0,numeroY=0;
+uint32_t m=0;
+uint32_t arregloX[20],arregloY[20];
+
+void ConfiguraUART_PC(void){//***************CONFIGURAR UART0********************//
 	unsigned long temp;
 	SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0;		// Se activa el reloj del UART
 	temp = SYSCTL_RCGC1_R;						// Espera de unos ciclos de reloj
@@ -35,7 +65,21 @@ void txcar_uart_PC(uint32_t car){				// Enviar caracter
 }
 uint8_t rxcar_uart_PC(void){					// Recibir caracter
 	uint8_t temp;
-	while ((UART0_FR_R & UART_FR_RXFE)!=0);		// Se espera que llegue un dato
+
+	while ((UART0_FR_R & UART_FR_RXFE)!=0){// Se espera que llegue un dato
+		if ((GPIO_PORTF_DATA_R&0x1)!=0x1){
+			while((GPIO_PORTF_DATA_R&0x1)!=0x1);
+			while((GPIO_PORTF_DATA_R&0x1)==0x1);
+			if (contadorCorte<=2)
+			{
+				txmens_uart_PC(msg6);				// Insuficiente
+			}else{
+				GPIO_PORTF_DATA_R=0x2;
+				break;
+				// Salimos del bucle
+			}
+		}
+	}
 	temp= UART0_DR_R&0xFF;						// Se toman solo 8 bits
 	return temp;
 }
@@ -49,33 +93,11 @@ void txmens_uart_PC(uint8_t mens[]){			// Similar a UART1
 		letra= mens[i++];
 	}
 }//***********FIN*CONFIGURAR UART0********************//
-
-int recibirNumero(void)
-{
-	uint32_t caracterRecibido;
-	uint32_t contador=0;
-	uint32_t numero=0;
-	uint8_t msg[]=" No se ingreso un numero! \n \r";
-	do{
-		caracterRecibido=rxcar_uart_PC();
-		if ((caracterRecibido<'0')&&(caracterRecibido>'9'))
-		{
-			txmens_uart_PC(msg);
-			break; 										// Si recibe cualquier otro valor
-		}else{
-			caracterRecibido-=30;						// Codigo ASCII
-			numero=numero*pow(10,contador)+caracterRecibido;// Algoritmo
-			contador++;
-		}
-	}while((caracterRecibido!='\n')&&(caracterRecibido!='\r'));
-
-	return numero;
-}
-
 void Config_SW_LEDS_PUERTOF(void){ 			//Configuramos puerto F sw1 sw2 LEDS
 	unsigned long temp;
-	SYSCTL_RCGCGPIO_R |= 0xFF;				// Se habilita la señal de reloj del Puerto F
+	SYSCTL_RCGCGPIO_R = 0xFF;				// Se habilita la señal de reloj del Puerto F
 	temp = SYSCTL_RCGC2_R;					// Espera a que se estabilice el reloj
+
 	GPIO_PORTF_LOCK_R = 0x4C4F434B;			//Quitamos proteccion
 	// Habilitar el bit0 (PF0 - pin protegido)
 	GPIO_PORTF_CR_R |= 0X1;					// Se habilita la modificación de varios registros del PF0
@@ -99,74 +121,68 @@ void retardo_ms (uint32_t milisegundos){
 	NVIC_ST_CTRL_R |= (NVIC_ST_CTRL_ENABLE + NVIC_ST_CTRL_CLK_SRC); 				//Habilitamos Systick
 	while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT)==0);
 }
-
+unsigned char Datos[3];
 void NumerotoString(uint16_t n){
 	/*
 	 * Esta función convierte un número a caracter ascii guardados en un arreglo
 	 */
-	Datos[0] = n/10000 + 0x30; 					// Decenas de miles
-	n = n%10000; 								// n ahora esta entre 0 y 9999
-	Datos[1] = n/1000 + 0x30; 					// Miles
-	Datos[2] = n/100 + 0x30; 					// Centenas
-	n = n%1000;									// n ahora esta entre 0 y 999
+	//Datos[0] = n/10000 + 0x30; 					// Decenas de miles
+	//n = n%10000; 								// n ahora esta entre 0 y 9999
+	//Datos[1] = n/1000 + 0x30; 					// Miles
+	//n = n%1000;									// n ahora esta entre 0 y 999
+	Datos[0] = n/100 + 0x30; 					// Centenas
 	n = n%100;									// n ahora esta entre 0 y 99
-	Datos[3] = n/10 + 0x30; 					// Decenas
+	Datos[1] = n/10 + 0x30; 					// Decenas
 	n = n%10; 									// n ahora esta entre 0 y 9
-	Datos[4] = n + 0x30;						// Unidades
+	Datos[2] = n + 0x30;						// Unidades
 }
-void main (void)
+
+int recibirNumero(void)
 {
-	ConfiguraUART_PC();							// Configura la comunicación PC
-	Config_SW_LEDS_PUERTOF();					// Configuramos las salidas
+	uint32_t caracterRecibido;
+	uint32_t contador=0;
+	uint32_t numero=0;
+	uint8_t msg[]="\n\rSE HA INGRESADO UN NUMERO! \n \r";
+	do{
+		caracterRecibido=rxcar_uart_PC();
+		if ((caracterRecibido==13)&&(caracterRecibido=='\r'))
+		{
+			txmens_uart_PC(msg);
+			break;
+		}else
+		{
+			caracterRecibido-=0x30;						// Codigo ASCII
+			numero=numero*10+caracterRecibido;// Algoritmo
+			contador++;
+		}
 
-	uint8_t msg1[]=" ¡BIENVENIDO! MAQUINA XY DE CORTE EN ESPERA DE COMANDO \n\r";
-	uint8_t msg2_1[]=" INTRODUZCA LA POSICIÓN ";
-	uint8_t msg2_2[]=" (X, Y) DEL POLÍGONO DE CORTE EN CM: \n\r";
-	uint8_t msg3_1[]=" SE INTRODUJO LA COORDENADA X = ";
-	uint8_t msg3_2[]=" CM, FALTA LA COORDENADA Y \n\r";
-	uint8_t msg4_1[]=" COORDENADA X INCORRECTA, VUELVA A INTRODUCIRLA \n\r";
-	uint8_t msg4_2[]=" COORDENADA Y INCORRECTA, VUELVA A INTRODUCIRLA \n\r";
-	uint8_t msg5_1[]=" POSICION ";
-	uint8_t msg5_2[]=" (";
-	uint8_t msg5_3[]=",";
-	uint8_t msg5_4[]=") CORRECTAMENTE INGRESADA \r\n";
-	uint8_t msg6[]=" NUMERO DE POSICIONES INSUFICIENTES PARA EJECUTAR EL CORTE\n\r";
+	}while(1);
+	return numero;
+}
+int main (void)
+{
+	ConfiguraUART_PC();
 
-	uint8_t msg7[]=" PROCESO DE CORTE INICIADO, CODIGO G: \n\r";
-	uint8_t msg8[]=" M03 \n\r";
-	uint8_t msg9[]=" M00 \n\r";
-	uint8_t msg11_1[]=" G00 X";
-	uint8_t msg11_2[]=" Y";
-	uint8_t msg11_3[]="\n\r";
-	uint8_t msg11_4[]=" G01 X";
-	uint8_t msg12_1[]=" G01 Z-1";
-	uint8_t msg12_2[]=" G01 Z1";
-	uint8_t msg13[]="N";
-	uint8_t fin[]=" PROCESO DE CORTE COMPLETADO CON EXITO\n\r";
-
-	uint32_t contadorCorte=1;
-	uint32_t banderaX=0,banderaY=0;
-	uint32_t cantMaxPosic=20;
-	int numeroX=0,numeroY=0;
-	uint32_t m=0;
-	uint32_t arregloX[cantMaxPosic],arregloY[cantMaxPosic];
-
-
-	while(1)
+	Config_SW_LEDS_PUERTOF();
+	contadorCorte=1;
+	while (1)
 	{
-		GPIO_PORTF_DATA_R=0x8;					// Prender led azul
+
+		GPIO_PORTF_DATA_R=0x4;					// Prender led azul
 		txmens_uart_PC(msg1);					// Texto Bienvenidos
-		while (GPIO_PORTA_DATA_R&0x10!=0);		// Mientras este presionado SW1
-		while (GPIO_PORTA_DATA_R&0x10==0x10);	// Mientras este suelto SW1
-		GPIO_PORTF_DATA_R=0x4;					// Prender led verde
-		while(contadorCorte<=cantMaxPosic&&contadorCorte>=2)
+		while ((GPIO_PORTF_DATA_R&0x10)==0x10);	// Mientras este suelto SW1
+		while ((GPIO_PORTF_DATA_R&0x10)!=0X0);		// Mientras este presionado SW1
+
+		GPIO_PORTF_DATA_R=0x8;					// Prender led verde
+		do
 		{
 
 			txmens_uart_PC(msg2_1);					// Texto INTRODUZCA POS
-			txcar_uart_PC(contadorCorte);			// Texto X
+			NumerotoString(contadorCorte);
+			txmens_uart_PC(Datos);
 			txmens_uart_PC(msg2_2);					// Texto termina msg2
 
-			while (banderaX==0&&banderaY==0)
+			while (banderaX==0||banderaY==0)
 			{
 				//	COORDENADA X
 				numeroX=recibirNumero();				// Recibimos numero
@@ -178,15 +194,16 @@ void main (void)
 					txmens_uart_PC(msg3_2);
 					banderaX=1;
 					//	COORDENADA Y
-					numeroY=recibirNumero();			// Recibimos numero
-					if ((numeroY>=0)&&(numeroY<=120))
-					{
-						banderaY=1;
-					}else{
-						txmens_uart_PC(msg4_2);			// Cordenada incorrecta
-					}
+
 				}else{
 					txmens_uart_PC(msg4_1);				// Cordenada incorrecta
+				}
+				numeroY=recibirNumero();			// Recibimos numero
+				if ((numeroY>=0)&&(numeroY<=120))
+				{
+					banderaY=1;
+				}else{
+					txmens_uart_PC(msg4_2);			// Cordenada incorrecta
 				}
 
 			}
@@ -194,8 +211,8 @@ void main (void)
 			banderaY=0;
 			// Guardamos en arreglos las posiciones X e Y
 
-			arregloX[contadorCorte--]=numeroX;
-			arregloY[contadorCorte--]=numeroY;
+			arregloX[contadorCorte-1]=numeroX;
+			arregloY[contadorCorte-1]=numeroY;
 
 
 			txmens_uart_PC(msg5_1);						// Posicion correctamente
@@ -210,19 +227,7 @@ void main (void)
 			txmens_uart_PC(msg5_4);
 			contadorCorte++;							// Siguiente posicion
 			retardo_ms(2000);
-			if (GPIO_PORTF_DATA_R&0x1==1){
-				while (GPIO_PORTF_DATA_R&0x1==1);
-				while(GPIO_PORTF_DATA_R&0x1!=0);
-				if (contadorCorte<=2)
-				{
-					txmens_uart_PC(msg6);				// Insuficiente
-				}else{
-					GPIO_PORTF_DATA_R=0x2;
-					break;
-					// Salimos del bucle
-				}
-			}
-		}
+		}while(1);
 
 		txmens_uart_PC(msg7);
 		//Empieza proceso de corte
